c89dc06b4ac3b6f3d036d11289549ec99590a6b8
Soroushby <sorosh.bayanati@yahoo.com>
2022-11-03 08:04:40 -0600

    implemented post and get with middleware

--- app.js
+++ app.js
@@ -4,8 +4,22 @@ const app = express();
 require("dotenv/config");
 
 const api = process.env.API_URL;
-app.get("/", (req, res) => {
-  res.send("hello api !");
+
+app.use(express.json());
+
+app.get(`${api}/doctors`, (req, res) => {
+  const doctors = {
+    id: 1,
+    name: "keyvan",
+    image: "some_url",
+  };
+  res.send(doctors);
+});
+
+app.post(`${api}/doctors`, (req, res) => {
+  const newDoctor = req.body;
+  console.log(newDoctor);
+  res.send(newDoctor);
 });
 
 app.listen(3000, () => {
