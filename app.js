92e5584dc04770864183656436edfe61edbab534
Soroushby <sorosh.bayanati@yahoo.com>
2022-11-03 07:29:24 -0600

    started edoctor app with node js

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+  res.send("hello api !");
+});
+
+app.listen(3000, () => {
+  console.log("server is running http://localhost:3000");
+});
