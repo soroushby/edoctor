cf9991fc45a4622b9512aa934900d68a96c65a37
Soroushby <sorosh.bayanati@yahoo.com>
2022-11-03 07:35:04 -0600

    added env and dotenv libary

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+
+require("dotenv/config");
+
+const api = process.env.API_URL;
 app.get("/", (req, res) => {
   res.send("hello api !");
 });
 
 app.listen(3000, () => {
+  console.log(api);
   console.log("server is running http://localhost:3000");
 });
